d5f559e271b4f8b81bba7be05b7a6b086231135a
calculusfish <SaskiaVanR@hotmail.com>
2017-08-25 21:19:12 +1000

    Added Basic Course and Class classes

--- classfile.py
+++ classfile.py
@@ -1,13 +1,40 @@
 class Course():
-    def __init__(self, code, classes):
+    def __init__(self, code, lectures, tutorials, practicals, workshops):
         self.code = code
-        self.classes = classes
+        self.lectures = lectures
+        self.tutorials = tutorials
+        self.practicals = practicals
+        self.workshops = workshops
+        self.weight = self.getWeight()
 
-
+    # gets the total number of hours required for the course per week
+    def getWeight(self):
+        time = 0
+        if len(lectures)!=0:
+            time += lectures[0].timeTotal
+        if len(tutorials)!=0:
+            time += tutorials[0].timeTotal
+        if len(practicals)!=0:
+            time += practicals[0].timeTotal
+        if len(workshops)!=0:
+            time += workshops[0].timeTotal
+        return time
+        
 
 class Class():
     def __init__(self, name, starts, ends, ctype, day):
         self.name = name
         self.starts = starts
         self.ends = ends
-        self.ctype
+        self.ctype = ctype
+        self.day = day
+        self.timeTotal = this.getTimeTotal()
+
+
+    # get the total number of hourse required for this class per week
+    def getTimeTotal(self):
+        i = 0
+        time = 0
+        while i < len(starts):
+            time +=ends[0]-starts[0]
+        return time
