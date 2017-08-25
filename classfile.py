d8e663ce835c705f38e397b49fd17345c05f7143
calculusfish <SaskiaVanR@hotmail.com>
2017-08-25 21:29:20 +1000

    Fixed Weighting Bugs and Added INFS1200
    Bug causing infinite loop in getTotalTime(). this instead of self. added
    small INFS1200 classes to test

--- classfile.py
+++ classfile.py
@@ -22,19 +22,34 @@ class Course():
         
 
 class Class():
-    def __init__(self, name, starts, ends, ctype, day):
+    def __init__(self, name, starts, ends, ctype, days):
         self.name = name
         self.starts = starts
         self.ends = ends
         self.ctype = ctype
-        self.day = day
-        self.timeTotal = this.getTimeTotal()
+        self.day = days
+        self.timeTotal = self.getTimeTotal()
 
 
     # get the total number of hourse required for this class per week
     def getTimeTotal(self):
         i = 0
         time = 0
-        while i < len(starts):
-            time +=ends[0]-starts[0]
+        while i < len(self.starts):
+            time +=self.ends[i]-self.starts[i]
+            i+=1
         return time
+
+
+lectures = []
+tutorials = []
+practicals = []
+workshops = []
+lectures +=[Class("L01", [12, 13], [14, 14], "L", [4,5])]
+tutorials +=[Class("T01", [15], [16], "T", [2])]
+tutorials +=[Class("T02", [9], [10], "T", [5])]
+tutorials +=[Class("T03", [10], [11], "T", [5])]
+
+infs1200 = Course("INFS1200", lectures, tutorials, practicals, workshops)
+print (infs1200.getWeight())           
+           
