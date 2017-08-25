8dc82606f88884dbd3780c1358ee00a12f7e7a58
calculusfish <SaskiaVanR@hotmail.com>
2017-08-25 21:07:41 +1000

    Initialize

---
+++ classfile.py
@@ -0,0 +1,13 @@
+class Course():
+    def __init__(self, code, classes):
+        self.code = code
+        self.classes = classes
+
+
+
+class Class():
+    def __init__(self, name, starts, ends, ctype, day):
+        self.name = name
+        self.starts = starts
+        self.ends = ends
+        self.ctype
