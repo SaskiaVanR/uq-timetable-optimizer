d4cdd64668579a3481b3eaccefa23123c980812f
Laine Schrader <laine@schrader.id.au>
2017-08-26 11:58:35 +1000

    data_interact.py: added does_course_exist function

--- modules/data_interact.py
+++ modules/data_interact.py
@@ -6,21 +6,32 @@ import os
 # It will eventually return the data as a Course class but for now it
 # just prints stuff
 def get_course_info(coursename):
-    try: coursejson = json.loads(open("../scraper/data/" + coursename).read())
+    try:
+        coursejson = json.loads(open("../scraper/data/" + coursename).read())
+        for i in range(len(coursejson['courses'][0]['activity_streams'])):
+            print(coursejson['courses'][0]['activity_streams'][i]['name'])
+            # Initialise empty array
+            times = []
+            for j in range(len(coursejson['courses'][0]['activity_streams']\
+                    [i]['events'])):
+                newtime = str(coursejson['courses'][0]['activity_streams'][i]\
+                        ['events'][j]['day']) + ": " + \
+                        str(coursejson['courses'][0]['activity_streams'][i]\
+                        ['events'][j]['start']\
+                        .split("T", 1)[1].split(":", 1)[0]) + " " \
+                        + str(int(coursejson['courses'][0]['activity_streams']\
+                        [i]['events'][j]['end']\
+                        .split("T", 1)[1].split(":", 1)[0]) + 1)
+                if newtime not in times:
+                    times.append(newtime)
+                    print(times[len(times) - 1])
     except: return None
-    for i in range(len(coursejson['courses'][0]['activity_streams'])):
-        print(coursejson['courses'][0]['activity_streams'][i]['name'])
-        # Initialise empty array
-        times = []
-        for j in range(len(coursejson['courses'][0]['activity_streams']\
-                [i]['events'])):
-            newtime = str(coursejson['courses'][0]['activity_streams'][i]\
-                    ['events'][j]['day']) + ": " + \
-                    str(coursejson['courses'][0]['activity_streams'][i]\
-                    ['events'][j]['start'].split("T", 1)[1].split(":", 1)[0]) \
-                    + " " \
-                    + str(int(coursejson['courses'][0]['activity_streams'][i]\
-                    ['events'][j]['end'].split("T", 1)[1].split(":", 1)[0]) + 1)
-            if newtime not in times:
-                times.append(newtime)
-                print(times[len(times) - 1])
+
+# Function that tells you if a course exists
+# It will check if it has been scraped
+# and return true or false
+def does_course_exist(coursename):
+    if os.path.exists("../scraper/data/" + coursename):
+        return True
+    else:
+        return False
