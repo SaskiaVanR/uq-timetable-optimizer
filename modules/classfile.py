6186abad9980fa3d2f777e72d5b0a1c408761535
Laine Schrader <laine@schrader.id.au>
2017-08-26 16:35:03 +1000

    removed unneeded whitespace, some commenting

--- modules/classfile.py
+++ modules/classfile.py
@@ -1,8 +1,8 @@
-
-alpha = ["A", "B", "C", "D", "E", "F", "G", "H" "I", "J" ,"K", "L", "M",\
-         "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
+# Global variables
+# Dictionary storing course names and associating them with course classes
 Courses = {}
+# alpha = ["A", "B", "C", "D", "E", "F", "G", "H" "I", "J" ,"K", "L", "M",\
+         # "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 # Class that stores information on a course
 # It contains the course code, and a bunch of streams
@@ -11,10 +11,6 @@ Courses = {}
 # at course.streams[ord("L") - ord("A")]. If there are not any streams with
 # The corresponding letter, the value will be 0
 class Course():
-
-    # this class stores the information for a single course
-    # a course will have a course code, and a list of streams
-
     def __init__(self, code, streams):
         #course code
         self.code = code
@@ -23,14 +19,12 @@ class Course():
         self.weight = self.getWeight()
         self.sortStreams(streams)
 
-
     #sorts the given list of streams into the correct index in self.streams
     def sortStreams(self, streams):
         for s in streams:
             if (self.streams[ord(s.name[0])-ord("A")])==0:
                 self.streams[ord(s.name[0])-ord("A")] = []
             self.streams[ord(s.name[0])-ord("A")] += [s]
-            
 
     # gets the total number of hours required for the course per week
     def getWeight(self):
@@ -62,7 +56,6 @@ class Stream():
         #total number of hours for this stream
         self.timeTotal = self.getTimeTotal()
 
-
     # get the total number of hours required for this class per week
     def getTimeTotal(self):
         i = 0
@@ -81,7 +74,6 @@ class Timetable():
         self.codes = codes
         self.streams = []
         self.assigned = self.makeAssignArray()
-
         self.needToAssign()
         
     def makeAssignArray(self):
@@ -91,25 +83,21 @@ class Timetable():
             array[i] = [0]*numCodes
         return array
 
-
     def addStream(self, stream):
-
         index = ord(stream.name[0]) - ord("A")
         codeIndex = self.getCodeIndex(stream.code)
-
         self.assigned[index][codeIndex] = 1
-
         self.streams +=[stream]
 
-
+    # Assigns -1 to the letters that do not need to be assigned for each course
+    # Stays 0 when it needs to be assigned
+    # Becomes 1 when assigned
     def needToAssign(self):
         for index, code in enumerate(self.codes):
             c = Courses[code]
             for i in range(26):
                 if c.streams[i]==0:
-                    self.assigned[index] = -1
-                    
-            
+                    self.assigned[i][index] = -1
 
     def makeCopy(self):
         t = Timetable(self.codes)
@@ -145,7 +133,6 @@ class Timetable():
             for i in range(len(s.starts)):
                 for time in range(s.starts[i],s.ends[i]):
                     hours[s.days[i]]+=[time]
-
         return hours
 
     def getWeight(self):
@@ -161,8 +148,6 @@ class Timetable():
         for i in range(5):
             starts[i] = starts[i]%24
             weight += ends[i] - starts[i]
-
-            
         return weight
 
     def getHWeight(self):
@@ -212,40 +197,27 @@ class Timetable():
 
 
 # def getTestTimetable():
-    
-
-
     # lectures = []
     # tutorials = []
     # practicals = []
     # lectures +=[Stream("INFS1200", "L01", [12, 13], [14, 14],[3,4])]
     # tutorials +=[Stream("INFS1200", "T01", [15], [16], [1])]
-
     # practicals +=[Stream("INFS1200", "P01", [9], [10], [3])]
-
-
     # infs1 = lectures + tutorials + practicals
-
     # infs1200 = Course("INFS1200", infs1)
     # #print (infs1200.getWeight())
-
-
     # lecturesm = []
     # tutorialsm = []
     # practicalsm = []
     # lecturesm +=[Stream("INFS2200", "L01", [12, 13], [14, 14], [1,2])]
     # tutorialsm +=[Stream("INFS2200", "T03", [10], [11], [3])]
     # practicalsm +=[Stream("INFS2200", "P02", [10], [11], [2])]
-
     # infs2 = lecturesm + tutorialsm + practicalsm
-
     # infs2200 = Course("INFS2200", infs2)
     # Courses = {"INFS1200": infs1200, "INFS2200": infs2200}
-
     # t = Timetable(["INFS1200", "INFS2200"])
     # streams = infs1 + infs2
     # t.addStreams(streams)
-
     # return t
 
 # Function that takes a list of courses and gets a dictionary from it
