cf334f0eefb4e26569029df7df9c09d374b0dc4f
Laine Schrader <laine@schrader.id.au>
2017-08-26 17:39:04 +1000

    cleanup

--- modules/classtestfile.py
+++
@@ -1,4 +0,0 @@
-from classfile import * 
-
-
-
--- modules/interface_interact.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-from classfile import Course
-from classfile import Stream
-from classfile import Timetable
-from data_interact import *
-
-
