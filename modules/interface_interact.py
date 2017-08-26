6befafc8268cb77a4b4ad36e69a6301754cce055
Laine Schrader <laine@schrader.id.au>
2017-08-26 15:43:29 +1000

    Fixed get_dictionary and moved to classfile

--- modules/interface_interact.py
+++ modules/interface_interact.py
@@ -6,12 +6,4 @@ from classfile import Stream
 from classfile import Timetable
 from data_interact import *
 
-# Function that takes a list of courses and gets a dictionary from it
-def get_dictionary(*coursecodes):
-    courses = []
-    Courses = {}
-    for i in courses:
-        course = get_course_info(i)
-        courses.append(course)
-        Courses[coursename] = course
-    return Courses
+
