1b269b24afa8c5f452230b7ebbc87e460d4de69b
Laine Schrader <laine@schrader.id.au>
2017-08-27 10:04:35 +1000

    helpers.py: Added function to get timetable as a 2D array

--- modules/helpers.py
+++ modules/helpers.py
@@ -65,3 +65,22 @@ def get_classes_from_stream(coursecode, streamname):
                         nextClass += ("-" + str(j.ends[k]))
                         classes.append(nextClass)
     return classes
+
+# Function that takes a Timetable class and returns a 2D Array of strings
+# Array entries are blank for free hours, and not blank for hours with classes
+# array[2][13] refers to the class on at 1pm on Wednesday
+def get_timetable_array(timetable):
+    # Create a 2D array for the timetable
+    timetablegrid = [[""] * 24] # one day
+    for i in range(7): # Seven days in a week
+        timetablegrid.append([""] * 24)
+    
+    # Fill the array
+    for i in timetable.streams:
+        # Go over each date/time combo in the stream
+        for j in range(len(i.days)):
+            for k in range(i.ends[j] - i.starts[j]):
+                # Add course code and stream name to array
+                timetablegrid[i.days[j]][i.starts[j] + k] \
+                        += i.code + " " + i.name
+    return timetablegrid
