a62b206bc61af9cd1d1048220f5bd5c2cf6daa88
Lucas <krecan.vox@gmail.com>
2017-08-27 15:11:45 +1000

    Updated a few colors and padding

--- interface/output_timetables.py
+++ interface/output_timetables.py
@@ -2,27 +2,31 @@ import sys
 import ctypes
 import tkinter as tk
 from tkinter import ttk
+from tkinter import font
 sys.path.append("../modules")
 import branchandbound
 import helpers
 
 
-def output_timetables(subject_codes):
+def output_timetables(subject_codes, max_hours, search_type):
 
     i = 0
     while i<len(subject_codes):
         subject_codes[i] = subject_codes[i].upper()
-        i+=1
+        i += 1
 
-    nodes = branchandbound.optimize(subject_codes)
+    nodes = []
+    if search_type == 1:
+        nodes = branchandbound.optimize(subject_codes, max_hours)
+    else:
+        nodes = branchandbound.optimizeDays(subject_codes, max_hours)
+        
     print(nodes[0].timetable.streams)
 
     window = tk.Tk()
     window.configure(background="white")
 
     window.title("Optimized Timetables")
-
-    note = ttk.Notebook(window)
 
     font_head = ""
     font_body = ""
@@ -34,41 +38,47 @@ def output_timetables(subject_codes):
     font_head_size = 16
     font_body_size = 12
 
+    ttk.Style().configure('.', font=font_body)
+
     frames = []
-
+    background_grid = []
     classes = []
-
     days = []
-
     times = []
-    numSolutions = len(nodes)
 
+    numSolutions = len(nodes)
 
     if len(nodes)>10:
         nodes = nodes[:10]
 
+    note = ttk.Notebook(window)
 
     for nindex, n in enumerate(nodes):
 
         frames += [ttk.Frame(note)]
         note.add(frames[nindex], text='Option '+str(nindex+1))
 
+        # background_grid += tk.Label(frames[nindex], text="")
+        # background_grid[-1].grid(row=0, column=0)
+        # background_grid[-1].configure(background="white")
+
         for sindex, s in enumerate(n.timetable.streams):
             for dindex, d in enumerate(s.days):
                 classes += [tk.Label(frames[nindex], text=s.code + '\n' + s.name + " " + str(s.starts[dindex]))]
 
                 classes[-1].grid(column=s.days[dindex]+1, row=s.starts[dindex]+1,
-                                     rowspan=s.ends[dindex]-s.starts[dindex], sticky="ns")
-
+                                     rowspan=s.ends[dindex]-s.starts[dindex], sticky="nsew", ipadx=5, ipady=5)
                 classes[-1].configure(background="white")
 
         for i in range(7):
             days += [tk.Label(frames[nindex], text=helpers.int_to_day(i))]
-            days[-1].grid(column=i+1, row=0)
+            days[-1].grid(column=i+1, row=0, sticky='nsew', ipadx=5, ipady=5)
+            days[-1].configure(background="white")
 
         for i in range(8,21):
             times += [tk.Label(frames[nindex], text=str(i)+":00\n")]
-            times[-1].grid(column=0, row=i+1)
+            times[-1].grid(column=0, row=i+1, sticky='nsew', ipadx=5, ipady=5)
+            times[-1].configure(background="white")
 
     note.pack()
 
